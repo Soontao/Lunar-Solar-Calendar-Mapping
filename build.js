fc180cbef976d271b0f59d8c93096aa2b00536b8
Theo Sun <theo.sun@outlook.com>
2019-03-25 21:34:43 +0800

    update logci

--- build.js
+++ build.js
@@ -6,12 +6,12 @@ const endDate = moment('2050-12-31');
 const { writeFileSync } = require("fs")
 
 
-var mappingString = "Solar Year,Solar Month,Solar Day,Lunar Year,Lunar Month,Is Leap,Lunar Day\n"
+var mappingString = "Solar Date,Lunar Date,Is Leap\n"
 
 
 for (var m = moment(startDate); m.isBefore(endDate); m.add(1, 'days')) {
     const result = solarLunar.solar2lunar(m.year(), m.month() + 1, m.date())
-    mappingString += `${result.cYear},${result.cMonth},${result.cDay},${result.lYear},${result.lMonth},${result.isLeap ? "Yes" : "No"},${result.lDay}\n`
+    mappingString += `${m.format("YYYY-MM-DD")},${result.lYear}-${result.lMonth}-${result.lDay},${result.isLeap ? "Yes" : "No"}\n`
 }
 
 writeFileSync("./bin/mapping.csv", mappingString, { encoding: "UTF-8" })
